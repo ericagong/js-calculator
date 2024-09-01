d074a670288b11be02f1e7c4296f2dbf465437f8
Erica Gong <the.erica.gong@gmail.com>
2024-09-02 05:59:48 +0900

    refactor: 테스트 코드 가독성을 위해 기능 단위 리팩토링

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -1,6 +1,6 @@
 import Calculator from '../src';
 
-describe('[Feature] 피연산자 두 개와 연산자 하나의 연산 결과를 반환한다.', () => {
+describe('[Feature2] 피연산자 두 개와 연산자 하나의 연산 결과를 반환한다.', () => {
     const calculator = new Calculator();
 
     describe('두 개의 피연산자에 대해 덧셈 결과를 반환한다.', () => {
@@ -62,63 +62,63 @@ describe('[Feature] 피연산자 두 개와 연산자 하나의 연산 결과를
                 );
             },
         );
-    });
 
-    describe('피연산자 중 0이 있는 경우, 올바른 연산 결과를 반환한다.', () => {
-        describe('첫 번째 피연산자는 0이 아니고, 두 번쨰 피연산자가 +0/-0인 경우, 나눗셈 결과 Infinity/-Infinity를 반환한다.', () => {
-            it.each([
-                { operand1: 30, operand2: +0 },
-                { operand1: -30, operand2: -0 },
-            ])(
-                `calculator.divide($operand1, $operand2) = +Infinity`,
-                ({ operand1, operand2 }) => {
-                    expect(calculator.divide(operand1, operand2)).toBe(
-                        Infinity,
-                    );
-                },
-            );
+        describe('피연산자 중 0이 있는 경우, 올바른 나눗셈 결과를 반환한다.', () => {
+            describe('첫 번째 피연산자만 0인 경우, 0을 반환한다.', () => {
+                it.each([
+                    { operand1: +0, operand2: 30 },
+                    { operand1: +0, operand2: -30 },
+                    { operand1: -0, operand2: 30 },
+                    { operand1: -0, operand2: -30 },
+                ])(
+                    `calculator.divide($operand1, $operand2) = 0`,
+                    ({ operand1, operand2 }) => {
+                        expect(calculator.divide(operand1, operand2)).toBe(
+                            operand1 / operand2,
+                        );
+                    },
+                );
+            });
 
-            it.each([
-                { operand1: 30, operand2: -0 },
-                { operand1: -30, operand2: +0 },
-            ])(
-                `calculator.divide($operand1, $operand2) = -Infinity`,
-                ({ operand1, operand2 }) => {
-                    expect(calculator.divide(operand1, operand2)).toBe(
-                        -Infinity,
-                    );
-                },
-            );
-        });
+            describe('두 번째 피연산자만 0인 경우, 연산자 부호 동일성 여부에 따라 Infinity/-Infinity를 반환한다.', () => {
+                it.each([
+                    { operand1: 30, operand2: +0 },
+                    { operand1: -30, operand2: -0 },
+                ])(
+                    `calculator.divide($operand1, $operand2) = +Infinity`,
+                    ({ operand1, operand2 }) => {
+                        expect(calculator.divide(operand1, operand2)).toBe(
+                            Infinity,
+                        );
+                    },
+                );
 
-        describe('첫 번째 피연산자가 0, 두 번쨰 피연산자가 0이 아닌 경우, 나눗셈 결과 0을 반환한다.', () => {
-            it.each([
-                { operand1: +0, operand2: 30 },
-                { operand1: +0, operand2: -30 },
-                { operand1: -0, operand2: 30 },
-                { operand1: -0, operand2: -30 },
-            ])(
-                `calculator.divide($operand1, $operand2) = 0`,
-                ({ operand1, operand2 }) => {
-                    expect(calculator.divide(operand1, operand2)).toBe(
-                        operand1 / operand2,
-                    );
-                },
-            );
-        });
+                it.each([
+                    { operand1: 30, operand2: -0 },
+                    { operand1: -30, operand2: +0 },
+                ])(
+                    `calculator.divide($operand1, $operand2) = -Infinity`,
+                    ({ operand1, operand2 }) => {
+                        expect(calculator.divide(operand1, operand2)).toBe(
+                            -Infinity,
+                        );
+                    },
+                );
+            });
 
-        describe('두 피연산자가 모두 0일 때, 나눗셈 결과 NaN을 반환한다.', () => {
-            it.each([
-                { operand1: +0, operand2: +0 },
-                { operand1: +0, operand2: -0 },
-                { operand1: -0, operand2: +0 },
-                { operand1: -0, operand2: -0 },
-            ])(
-                `calculator.divide($operand1, $operand2) = NaN`,
-                ({ operand1, operand2 }) => {
-                    expect(calculator.divide(operand1, operand2)).toBeNaN();
-                },
-            );
+            describe('두 피연산자가 모두 0인 경우, NaN을 반환한다.', () => {
+                it.each([
+                    { operand1: +0, operand2: +0 },
+                    { operand1: +0, operand2: -0 },
+                    { operand1: -0, operand2: +0 },
+                    { operand1: -0, operand2: -0 },
+                ])(
+                    `calculator.divide($operand1, $operand2) = NaN`,
+                    ({ operand1, operand2 }) => {
+                        expect(calculator.divide(operand1, operand2)).toBeNaN();
+                    },
+                );
+            });
         });
     });
 });
